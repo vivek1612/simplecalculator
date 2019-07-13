783db6abaf45f2f6f8c87d8900360cb3625a2854
vivek <vivek1612@gmail.com>
2019-07-14 00:21:22 +0530

    cleanup

--- src/com/vivek/simplecalculator/SimpleCalculator.java
+++ src/com/vivek/simplecalculator/SimpleCalculator.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 public class SimpleCalculator {
 
 
-    private static final String QUIT = "quit";
-    private static final String PROMT = ">";
-    private static final String HELP = "help";
+    private static final String CMD_PROMT = ">";
+    private static final String CMD_QUIT = "quit";
+    private static final String CMD_HELP = "help";
     private static final String HELP_MSSG =
             "Supported operations: add(+), subtract(-), " +
                     "divide(/), multiply(*), modulus(%), power(^)\n" +
@@ -24,11 +24,11 @@ public class SimpleCalculator {
         String input;
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
         do {
-            System.out.print(PROMT);
+            System.out.print(CMD_PROMT);
             input = scanner.nextLine();
-            if (input.equalsIgnoreCase(QUIT)) {
+            if (input.equalsIgnoreCase(CMD_QUIT)) {
                 System.exit(0);
-            } else if (input.equalsIgnoreCase(HELP)) {
+            } else if (input.equalsIgnoreCase(CMD_HELP)) {
                 System.out.println(HELP_MSSG);
                 continue;
             }
